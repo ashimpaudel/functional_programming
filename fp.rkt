401ef4b042f8fdeb1ba74c90c748c9078e4fa279
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-01-20 11:30:47 -0500

    second commit

---
+++ fp.rkt
@@ -0,0 +1,14 @@
+;;; The reverse-general procedure reverses the list
+(DEFINE (reverse-general lis)
+        ;figuring out an way to get the last element of list
+        ; CDR of list. CAR of list in recursive manner
+        (COND
+           ((NULL? lis) lis) ; if the list is null simply return null list
+           (ELSE (CONS (reverse-general (CDR lis))
+                  
+                  (CAR lis) 
+                  )
+           )
+         )
+      )
+        
