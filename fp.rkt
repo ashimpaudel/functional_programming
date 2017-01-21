dfd1edd4cd730cb96468ecb006a8c4aa3532435a
Ashimpaudel <ashim.paudel@bison.howard.edu>
2017-01-21 11:58:01 -0500

    completion of procedure reverse-general

--- fp.rkt
+++ fp.rkt
@@ -1,33 +1,21 @@
-(DEFINE (reverse-sim list1)
-        (COND
-         ((NULL? list1) list1)
-         (ELSE (append (reverse-sim (CDR list1))
-                       (list (CAR list1)))
-               )
-         )
-        )
-;;; The reverse-general procedure reverses the list
+;;;Problem 1. The reverse-general procedure reverses the list
 (DEFINE (reverse-general lis)
         ;figuring out an way to get the last element of list
-        ; CDR of list. CAR of list in recursive manner
+        ; logic = (CDR of list) (CAR of list) in recursive manner and appending them
         (COND
            ((NULL? lis) lis) ; if the list is null simply return null list
            (ELSE 
-            (IF  (LIST? (CAR lis))
+                (IF  (LIST? (CAR lis)) ; if the first item of list is also list
                 
-            ( append  (reverse-general (CDR lis))
-                  
-                  ; if CAR function produce a list itself:
-                  ; we have to apply reverse-general to it as well
-                  ; using IF to
-                         
-                     (list(reverse-general (CAR lis))) ; if not used list at front will remove every other braces except first and last
+                     ( append  (reverse-general (CDR lis)) ; apply logic by recursively calling reverse function to both CDR an CAR
+                               (list(reverse-general (CAR lis))) ; appending reverse of CDR and reverse of CAR
+                               )
                               
-                  )
-            (append (reverse-general (CDR lis)) 
-                    (list (CAR lis)))
-            
-           )
-         )
-      )
-        )
+                  
+                     (append (reverse-general (CDR lis)) ; else first item of list is not a list
+                             (list (CAR lis))            ;append reverse of CDR with list of CAR
+                             )
+                      )
+                 )
+             )
+          )
